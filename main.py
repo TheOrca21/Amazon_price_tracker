419aecfea0401f307d5f26679eb9c9943b9b638c
Sudhakar_Sridharan <117033022+TheOrca21@users.noreply.github.com>
2023-12-16 21:13:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import smtplib
 import os
 
 header = {'Accept-Language': 'en-US', 'User-Agent': 'Mozilla'}
-response = requests.get('https://www.amazon.in/Dell-Alienware-i9-12900H-39-62Cms-D569942WIN9/dp/B09TP13BDV/ref=sr_1_4?crid=1E4YFVWT35DYB&keywords=dell+alienware+laptop&qid=1702739709&sprefix=dell+a%2Caps%2C275&sr=8-4', headers=header).text
+response = requests.get(URL, headers=header).text
 soup = BeautifulSoup(response, 'lxml')
 price = soup.find(name='span', class_='a-price-whole').getText()
 price = int(str(price).replace(',', '').replace('.', ''))
-msg = f'Hey the Alien-ware price has dropped to {price}!'
+msg = f'Hey price has dropped to {price}!'
 if price < 330000:
     email = os.environ.get('email')
     password = os.environ.get('password')
